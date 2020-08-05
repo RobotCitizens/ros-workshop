3479b20066034f138e550c6d452797320b5a0b13
RobotCitizens Research Laboratory <69115549+RobotCitizens@users.noreply.github.com>
2020-08-05 07:24:18 +0700

    Create dynamic_talker.py

---
+++ dynamic_talker.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = rospy.get_param("talker_str")
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
